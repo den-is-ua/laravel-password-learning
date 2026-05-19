24e6fcc9a90149e9223d2fe82c902590db038e69
admin <admin@admins-MacBook-Pro.local>
2026-05-19 09:36:07 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Models\Account;
+use Laravel\Passport\Http\Middleware\CheckToken;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
